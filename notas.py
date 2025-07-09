6126134f5bc607d2838bf2ba30879e2652f03015
Cassio Pereira <profcassioribeiro@gmail.com>
2025-07-09 18:30:05 -0300

    apagar notas

--- notas.py
+++
@@ -1,7 +0,0 @@
-nome = input('Digite o nome do aluno: ')
-nota1 = float(input('Digite a nota do primeiro bimestre: '))
-nota2 = float(input('Digite a nota do segundo bimestre: '))
-
-media = (nota1 + nota2) / 2
-
-print(f'A média do aluno {nome} é {media}')
